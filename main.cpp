abd00c7888e26b2f11b0f55b98e1c4999368efba
emanuelcortez32 <emanuelcortez32@gmail.com>
2017-11-07 15:40:50 -0300

    Primer commit
    Juegos SplixIO en C++ para Paradigmas de Programacion II

---
+++ main.cpp
@@ -0,0 +1,129 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <time.h>
+#include <iostream>
+
+#define M 20
+#define N 20
+
+using namespace sf;
+
+int campo[M][N] = {0};
+
+int ts = 18;
+
+void pintar(int y,int x)
+{
+  if (campo[y][x]==0) campo[y][x]=-1;
+  if (campo[y-1][x]==0) pintar(y-1,x);
+  if (campo[y+1][x]==0) pintar(y+1,x);
+  if (campo[y][x-1]==0) pintar(y,x-1);
+  if (campo[y][x+1]==0) pintar(y,x+1);
+}
+
+
+int main()
+{
+    //Logica
+
+    srand(time(0));
+    RenderWindow window(VideoMode(M*ts,N*ts), "SplixIO");
+    window.setFramerateLimit(60);
+
+    Texture t1,t2;
+
+    t1.loadFromFile("images/tiles.png");
+    t2.loadFromFile("images/gameover.png");
+
+    Sprite sTile(t1), sGameOver(t2);
+
+    float timer=0, delay=0.07;
+    Clock clock;
+
+    int x=9, y=9, dx=0, dy=0;
+
+    for(int i = 0; i<M; i++){
+        for(int j = 0; j< N; j++){
+            if(i == 8 && j == 8) campo[i][j] = 1;
+            if(i == 8 && j == 9) campo[i][j] = 1;
+            if(i == 8 && j == 10) campo[i][j] = 1;
+            if(i == 9 && j == 8) campo[i][j] = 1;
+            if(i == 9 && j == 9) campo[i][j] = 1;
+            if(i == 9 && j == 10) campo[i][j] = 1;
+            if(i == 10 && j == 8) campo[i][j] = 1;
+            if(i == 10 && j == 9) campo[i][j] = 1;
+            if(i == 10 && j == 10) campo[i][j] = 1;
+        }
+    }
+
+    while(window.isOpen()){
+
+        float time = clock.getElapsedTime().asSeconds();
+		clock.restart();
+		timer+=time;
+
+        Event e;
+
+        while(window.pollEvent(e)){
+            if(e.type == Event::Closed){
+                window.close();
+            }
+
+            if(e.type == Event::KeyPressed){
+                if(e.key.code == Keyboard::Escape){
+                    window.close();
+                }
+            }
+        }
+
+        if (Keyboard::isKeyPressed(Keyboard::Left)) {dx=-1;dy=0;}
+	    if (Keyboard::isKeyPressed(Keyboard::Right))  {dx=1;dy=0;}
+	    if (Keyboard::isKeyPressed(Keyboard::Up)) {dx=0;dy=-1;}
+		if (Keyboard::isKeyPressed(Keyboard::Down)) {dx=0;dy=1;}
+
+		if (timer>delay)
+		{
+         x+=dx;
+         y+=dy;
+
+		 if (x<0) x=0; if (x>N-1) x=N-1;
+         if (y<0) y=0; if (y>M-1) y=M-1;
+
+         if (campo[y][x]==2) return 0;
+         if (campo[y][x]==0) campo[y][x]=2;
+         if (campo[x][y]==1) {
+            for(int i = 0; i < M; i++){
+                for(int j = 0; j< N; j++){
+                    if(campo[i][j]==0){
+                        if(campo[i][j-1] == 2 && campo[i-1][j] == 2){
+                            pintar(i,j);
+                            campo[i][j] = 1;
+                        }
+                    }
+                }
+            }
+         }
+         timer=0;
+		}
+
+        ///draw
+        window.clear();
+
+        for(int i = 0; i<M; i++){
+            for(int j = 0; j<N; j++){
+                if(campo[i][j] == 0) continue;
+                if(campo[i][j] == 1) sTile.setTextureRect(IntRect(0,0,ts,ts));
+                sTile.setPosition(j*ts,i*ts);
+                window.draw(sTile);
+            }
+        }
+
+        sTile.setTextureRect(IntRect(0,0,ts,ts));
+        sTile.setPosition(x*ts,y*ts);
+        window.draw(sTile);
+
+        window.display();
+    }
+
+    return 0;
+}
